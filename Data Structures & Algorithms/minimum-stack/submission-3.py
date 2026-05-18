23004eb6e50d213c6cc92149e1a6656887b0494e
Ryan Dwyer <56858797+rydwy01@users.noreply.github.com>
2026-05-18 06:42:34 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.prevMinIdxs = []
+        self.stack = []
+        # self.prevMinIdx = 0
+        self.curMinIdx = -1
+
+    def push(self, val: int) -> None:
+        #If first one, append and set curMinIdx to 0, otherwise compare to prev min
+        self.stack.append(val)
+        if len(self.stack) > 1:
+            if self.stack[-1] < self.stack[self.curMinIdx]:
+                self.prevMinIdxs.append(self.curMinIdx)
+                self.curMinIdx = len(self.stack) - 1
+        else:
+            self.curMinIdx = 0
+
+
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        if not self.stack:
+            self.curMinIdx = -1
+        elif self.curMinIdx == len(self.stack):
+            self.curMinIdx = self.prevMinIdxs.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.stack[self.curMinIdx]
+        
